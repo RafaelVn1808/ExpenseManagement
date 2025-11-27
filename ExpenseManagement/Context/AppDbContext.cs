be1a91129d12d6c24427d5ae9f6d79a7e7429966
rafael.almeida <rafaelalmeida1808@gmail.com>
2025-11-27 15:31:40 -0300

    Preprando o migrations e começando a utilizar o data annotations

---
+++ ExpenseManagement/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ExpenseManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseManagement.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {    
+        }
+
+        public DbSet<Category>? Category { get; set; }
+        public DbSet<Expense>? Expense { get; set; }
+    }
+}
