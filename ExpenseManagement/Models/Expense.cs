f74281bbae9599c176d2ef92782a852d5ba9e8cb
rafael.almeida <rafaelalmeida1808@gmail.com>
2025-11-27 12:25:46 -0300

    Adicionar arquivos de projeto.

---
+++ ExpenseManagement/Models/Expense.cs
@@ -0,0 +1,22 @@
+﻿namespace ExpenseManagement.Models
+{
+    public class Expense{
+        
+        public int ExpenseId { get; set; }
+        public DateTime Date { get; set; }
+        public required string Name { get; set; }
+        public required string Category { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Validity { get; set; }
+        public int Installments { get; set; }
+        public required string Status { get; set; }
+        public string? NoteImageUrl { get; set; }
+        public string? ProofImageUrl { get; set; }
+
+
+
+
+
+
+    }
+}
