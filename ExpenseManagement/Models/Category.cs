975c1c7a3330d06f36e4d170955be9738119f685
rafael.almeida <rafaelalmeida1808@gmail.com>
2025-11-27 15:31:40 -0300

    Preprando o migrations e começando a utilizar o data annotations

---
+++ ExpenseManagement/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseManagement.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            Expenses = new Collection<Expense>();
+        }
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        [StringLength(80)]
+        public required string Name { get; set; }
+
+        public ICollection<Expense>? Expenses { get; set; }
+
+    }
+}
