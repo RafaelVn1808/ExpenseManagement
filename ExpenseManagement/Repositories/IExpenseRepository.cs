801ee0d4cabe8b0ad09c4f313f359495eb574e64
rafael.almeida <rafaelalmeida1808@gmail.com>
2025-12-03 15:38:39 -0300

    implementação do expenseRepository

--- ExpenseManagement/Repositories/IExpenseRepository.cs
+++ ExpenseManagement/Repositories/IExpenseRepository.cs
@@ -4,6 +4,12 @@ namespace ExpenseManagement.Repositories
 {
     public interface IExpenseRepository
     {
-        
+
+        IEnumerable<Expense> GetExpenses();
+        Expense GetExpenseId(int expenseId);
+        Expense Create(Expense expense);
+        Expense Update(Expense expense);
+        Expense Delete(int expenseId);
+
     }
 }
