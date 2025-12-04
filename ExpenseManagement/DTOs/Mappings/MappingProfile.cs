f06794fb1a63d2e0208febb01d649615ba4b1e19
rafael.almeida <rafaelalmeida1808@gmail.com>
2025-12-04 16:06:08 -0300

    .

---
+++ ExpenseManagement/DTOs/Mappings/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ExpenseManagement.Models;
+
+namespace ExpenseManagement.DTOs.Mappings
+{
+    public class MappingProfile : Profile
+    {
+
+        public MappingProfile()
+        {
+            CreateMap<Expense, ExpenseDTO>().ReverseMap();
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+
+        }
+    }
+}
