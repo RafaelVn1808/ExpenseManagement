bddd538c63802bb6b90196463183623972730c3c
rafael.almeida <rafaelalmeida1808@gmail.com>
2025-12-05 16:14:39 -0300

    ,,

--- ExpenseManagement/Services/CategoryService.cs
+++ ExpenseManagement/Services/CategoryService.cs
@@ -1,4 +1,6 @@
-﻿using ExpenseManagement.DTOs;
+﻿using AutoMapper;
+using ExpenseManagement.DTOs;
+using ExpenseManagement.Models;
 using ExpenseManagement.Repositories;
 
 namespace ExpenseManagement.Services
@@ -6,36 +8,45 @@ namespace ExpenseManagement.Services
     public class CategoryService : ICategoryService
     {
         private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
 
-        public CategoryService(ICategoryRepository categoryRepository)
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
         {
             _categoryRepository = categoryRepository;
+            _mapper = mapper;
         }
 
-        public Task<IEnumerable<CategoryDTO>> GetCategories()
+        public async Task<IEnumerable<CategoryDTO>> GetCategories()
         {
-            throw new NotImplementedException();
+            var categoriesEntity = await _categoryRepository.GetCategories();
+            return _mapper.Map<IEnumerable<CategoryDTO>>(categoriesEntity);
         }
 
-        public Task<CategoryDTO> GetCategoryById(int categoryId)
+        public async Task<CategoryDTO> GetCategoryById(int categoryId)
         {
-            throw new NotImplementedException();
+            var categoriesEntity = await _categoryRepository.GetCategoryById(categoryId);
+            return _mapper.Map<CategoryDTO>(categoriesEntity);
         }
-        public Task<CategoryDTO> AddCategory(CategoryDTO category)
+        public async Task AddCategory(CategoryDTO category)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<CategoryDTO> UpdateCategory(CategoryDTO category)
-        {
-            throw new NotImplementedException();
+            var categoryEntity = _mapper.Map<Category>(category);
+            await _categoryRepository.Create(categoryEntity);
+            category.CategoryId = categoryEntity.CategoryId;
         }
 
-        public Task<CategoryDTO> RemoveCategory(int categoryId)
+        public async Task UpdateCategory(CategoryDTO categoryDto)
         {
-            throw new NotImplementedException();
+            var categoryEntity = _mapper.Map<Category>(categoryDto);
+            await _categoryRepository.Update(categoryEntity);
         }
 
-        
+        public async Task RemoveCategory(int categoryId)
+        {
+            var categoryEntity = _categoryRepository.GetCategoryById(categoryId).Result;
+            await _categoryRepository.Delete(categoryEntity.CategoryId);
+
+        }
+
+       
     }
 }
---
+++ ExpenseManagement/Services/IExpenseService.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseManagement.DTOs;
+
+namespace ExpenseManagement.Services
+{
+    public interface IExpenseService
+    {
+        Task<IEnumerable<ExpenseDTO>> GetAllProductsAsync();
+        Task GetProductByIdAsync(int id);
+        Task CreateProductAsync(ExpenseDTO productDto);
+        Task UpdateProductAsync(int id, ExpenseDTO productDto);
+        Task DeleteProductAsync(int id);
+
+    }
+}
