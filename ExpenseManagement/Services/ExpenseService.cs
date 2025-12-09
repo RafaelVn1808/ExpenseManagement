a0170df86aa8bd698e90ac649925914a91d8899c
rafael.almeida <rafaelalmeida1808@gmail.com>
2025-12-09 11:22:51 -0300

    implementando os Expense services

---
+++ ExpenseManagement/Services/ExpenseService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using ExpenseManagement.DTOs;
+using ExpenseManagement.Models;
+using ExpenseManagement.Repositories;
+
+namespace ExpenseManagement.Services
+{
+    public class ExpenseService : IExpenseService
+    {
+        private readonly IExpenseRepository _expenseRepository;
+        private readonly IMapper _mapper;
+
+        public ExpenseService(IExpenseRepository expenseRepository, IMapper mapper)
+        {
+            _expenseRepository = expenseRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ExpenseDTO>> GetAllExpensesAsync()
+        {
+            var expensesEntity = await _expenseRepository.GetExpenses();
+            return _mapper.Map<IEnumerable<ExpenseDTO>>(expensesEntity);
+        }
+
+        public async Task<ExpenseDTO> GetExpensesByIdAsync(int id)
+        {
+            var expensesEntity = await _expenseRepository.GetExpenseId(id);
+            return _mapper.Map<ExpenseDTO>(expensesEntity);
+            
+        }
+
+        public async Task CreateExpensesAsync(ExpenseDTO expenseDto)
+        {
+            var expenseEntity = _mapper.Map<Expense>(expenseDto);
+            await _expenseRepository.Create(expenseEntity);
+            expenseDto.Category = expenseEntity.Category;
+        }
+
+        public async Task UpdateExpenseAsync(ExpenseDTO expenseDto)
+        {
+            var categoryEntity = _mapper.Map<Expense>(expenseDto);
+            await _expenseRepository.Update(categoryEntity);
+        }
+
+        public async Task DeleteExpenseAsync(int id)
+        {
+            var expenseEntity = _expenseRepository.GetExpenseId(id).Result;
+            await _expenseRepository.Delete(expenseEntity.ExpenseId);
+        }
+    }
+}
