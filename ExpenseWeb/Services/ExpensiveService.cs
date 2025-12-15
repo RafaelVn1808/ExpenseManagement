fc907d8f2702e2930061963169c46e4ada1b9327
rafael.almeida <rafaelalmeida1808@gmail.com>
2025-12-15 14:53:42 -0300

    Começando a aplicação que consome

---
+++ ExpenseWeb/Services/ExpensiveService.cs
@@ -0,0 +1,34 @@
+﻿using ExpenseWeb.Models;
+using ExpenseWeb.Services.Contracts;
+
+namespace ExpenseWeb.Services
+{
+    public class ExpensiveService : IExpensiveService
+    {
+        public Task<IEnumerable<ExpenseViewModel>> GetAllExpenses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ExpenseViewModel> GetExpenseById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ExpenseViewModel> CreateExpense(ExpenseViewModel expense)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ExpenseViewModel> UpdateExpense(int id, ExpenseViewModel expense)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteExpense(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
