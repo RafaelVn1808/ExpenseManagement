6afe617f4a358961d68a83dbc11d2f24daaf8a23
Rafael Vinicius dos Reis Almeida <rafaelalmeida1808@gmail.com>
2025-12-16 01:03:33 -0300

    Implementando as views

---
+++ ExpenseWeb/Controllers/ExpenseController.cs
@@ -0,0 +1,27 @@
+﻿using ExpenseWeb.Models;
+using ExpenseWeb.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseWeb.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly IExpenseService _expenseService;
+
+        public ExpenseController(IExpenseService expenseService)
+        {
+            _expenseService = expenseService;
+        }
+
+        public async Task<ActionResult<IEnumerable<ExpenseViewModel>>> Index()
+        {
+            var result = await _expenseService.GetAllExpenses();
+
+            if (result is null) { 
+                return View("Error");
+            }
+
+            return View(result);
+        }
+    }
+}
