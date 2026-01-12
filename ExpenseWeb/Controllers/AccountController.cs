79558a8074b04d0f27a09ea9fa39b3ef2be6a4d2
rafael.almeida <rafaelalmeida1808@gmail.com>
2026-01-12 14:52:41 -0300

    Começando a implementar a autenticação

---
+++ ExpenseWeb/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using ExpenseWeb.Models;
+using ExpenseWeb.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AccountController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var token = await _authService.Login(model);
+
+            if (token == null)
+            {
+                ModelState.AddModelError("", "Login inválido");
+                return View(model);
+            }
+
+            HttpContext.Session.SetString("JWToken", token);
+
+            return RedirectToAction("Index", "Expense");
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ ExpenseWeb/Services/Contracts/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using ExpenseWeb.Models;
+
+namespace ExpenseWeb.Services.Contracts
+{
+    public interface IAuthService
+    {
+        Task<string?> Login(LoginViewModel model);
+    }
+}
